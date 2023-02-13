03c01e476501507b133ff2dfd58147a9b2c0fdca
ole <olefha@uio.no>
2023-02-13 17:54:59 +0100

    Created Rick and Morty Project

---
+++ RickAndMorty/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacter.swift
+//  RickAndMorty
+//
+//  Created by Ole Hagemann on 07/02/2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Ole Hagemann on 07/02/2023.
+//
+
+import Foundation
